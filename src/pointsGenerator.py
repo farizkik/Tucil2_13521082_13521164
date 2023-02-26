9ae28a82b8b6f6446303a077601069230543eaa5
blixa-rd <16521074@mahasiswa.itb.ac.id>
2023-02-27 01:47:24 +0700

    add random points generator

---
+++ src/pointsGenerator.py
@@ -0,0 +1,17 @@
+import random
+
+def generateRandomPoints():
+    n = int(input("How many points to be randomly generated: ")) # number of points to generate
+    N = int(input("Input the dimension: ")) # number of dimension (in space R^N)
+    points = []
+
+    for i in range(n):
+        point = []
+        for j in range(N):
+            # random float between 1000000 and 1000000
+            # array representing the point, rounded to 3 floating numbers
+            point.append(round(random.uniform(-10**6, 10**6), 3))
+        points.append(point)
+    return points
+
+print(generateRandomPoints())
