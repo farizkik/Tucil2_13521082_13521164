c375d36cb331c61879f9168cb614b43a181536d8
blixa-rd <16521074@mahasiswa.itb.ac.id>
2023-02-27 22:53:35 +0700

    add main

--- src/main.py
+++ src/main.py
@@ -2,22 +2,67 @@ from DNC import *
 from bruteForce import *
 from pointsGenerator import *
 from utilities import *
+import time
 
 points, dimension = generateRandomPoints()
 
-print(bruteForceClosestPairnD(points))
-print(closestPairnD(points))
+# Finding Closest Pair with Brute Force Approach
+start = time.time()
+result = bruteForceClosestPairnD(points)
+end = time.time()
 
-'''
+print("\nWith Brute Force Approach:")
+print("the closest pair are between:", end=" ")
+print(result[1])
+print("with the minimum distance:", round(result[0], 3))
+print("the euclidean operation executed by:", result[2], "times")
+print("and execution time within:", end=" ")
+print("{0:.10f}".format(end-start), "seconds")
+
+
+# Finding Closest Pair with Divide n Conquer Approach
+start = time.time()
 result = closestPairnD(points)
-minDist = result[0]
-closest_pair = result[1]
+end = time.time()
 
+print("\nWith Divide and Conquer Approach:")
+print("the closest pair are between:", end=" ")
+print(result[1])
+print("with the minimum distance:", round(result[0], 3))
+print("the euclidean operation executed by:", result[2], "times")
+print("and execution time within:", end=" ")
+print("{0:.10f}".format(end-start), "seconds\n")
 
+# Asking for visualization
 if (dimension == 3):
-    visualize3D(points, closest_pair)
+    while True:
+        visualize = input("Do you want to visualize the points? (y/n): ")
+        if (visualize == 'y'):
+            visualize3D(points, result[1])
+            break
+        elif (visualize == 'n'):
+            break
+        else:
+            print("Invalid input!")
+
 elif (dimension == 2):
-    visualize2D(points, closest_pair)
+    while True:
+        visualize = input("Do you want to visualize the points? (y/n): ")
+        if (visualize == 'y'):
+            visualize2D(points, result[1])
+            break
+        elif (visualize == 'n'):
+            break
+        else:
+            print("Invalid input!")
+
 elif (dimension == 1):
-    visualize1D(points, closest_pair)
-    '''
+    while True:
+        visualize = input("Do you want to visualize the points? (y/n): ")
+        if (visualize == 'y'):
+            visualize1D(points, result[1])
+            break
+        elif (visualize == 'n'):
+            break
+        else:
+            print("Invalid input!")
