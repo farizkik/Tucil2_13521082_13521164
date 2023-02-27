50ad1520fe3ec0553f4849de9f28ab8fdb86980a
blixa-rd <16521074@mahasiswa.itb.ac.id>
2023-02-27 21:24:06 +0700

    add visualize testing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
 from DNC import *
 from bruteForce import *
 from pointsGenerator import *
+from utilities import *
 
-points=generateRandomPoints()
-print(bruteForceClosestPairnD(points))
-print(closestPairnD(points))
+points, dimension = generateRandomPoints()
+
+# print(bruteForceClosestPairnD(points))
+# print(closestPairnD(points))
+
+result = closestPairnD(points)
+minDist = result[0]
+closest_pair = result[1]
+
+if (dimension == 3):
+    visualize3D(points, closest_pair)
+elif (dimension == 2):
+    visualize2D(points, closest_pair)
+elif (dimension == 1):
+    visualize1D(points, closest_pair)
